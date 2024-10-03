58aa5591e62e2fcf20d725067482b4a5014e8005
Raunak-Xm89 <raunak.magar@sifal.deerwalk.edu.np>
2024-10-03 16:00:16 +0545

    Binary Search

---
+++ src/main/java/org/example/PeakMountainIndex.java
@@ -0,0 +1,21 @@
+package org.example;
+//Question Link : https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+public class PeakMountainIndex {
+    public static void main(String[] args) {
+        int [] arr = {0,10,5,2};
+        System.out.println(PeakSearch(arr));
+    }
+    static int PeakSearch(int [] arr){
+        int s = 0, e = arr.length-1; //Defining start and end
+        int m;
+        while (s < e){
+            m = s+(e-s)/2; // finding out the middle element
+            if (arr[m]> arr[m+1]){ // checking if the element is peak
+               e = m;
+            } else {
+                s= m+1;
+            }
+        }
+        return s;
+    }
+}

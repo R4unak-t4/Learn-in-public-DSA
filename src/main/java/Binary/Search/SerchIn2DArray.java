a0029571122403ff476acb0a912a486d02ecec20
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-06 13:23:28 +0545

    Binary Search

--- src/main/java/Binary/Search/SerchIn2DArray.java
+++ src/main/java/Binary/Search/SerchIn2DArray.java
@@ -3,6 +3,24 @@ package Binary.Search;
 
 public class SerchIn2DArray {
     public static void main(String[] args) {
-
+        int [][] matrix = {{1,3,5,7},
+                {10,11,16,20},
+                {23,30,34,60}};
+        int target = 3;
+        System.out.println(BinarySearch(matrix,target));
+    }
+    static Boolean BinarySearch(int [][] matrix, int target){
+        int row = 0;
+        int col = matrix[row].length-1;
+        while (row <= matrix.length-1 && col >=0){
+            if (matrix[row][col] == target){
+                return true;
+            } else if (matrix[row][col]>target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
     }
 }

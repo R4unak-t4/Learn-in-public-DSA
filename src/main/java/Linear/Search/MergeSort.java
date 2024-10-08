af179d9fbdce227cf70365f31c10e9ed195d9302
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-08 19:26:22 +0545

    Binary Search

---
+++ src/main/java/Linear/Search/MergeSort.java
@@ -0,0 +1,35 @@
+package Linear.Search;
+
+import java.util.Arrays;
+
+//Question: https://leetcode.com/problems/merge-sorted-array/description/
+public class MergeSort {
+    public static void main(String[] args) {
+        int [] nums1 = {1,2,3,0,0,0};
+        int [] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+        System.out.println(Arrays.toString(Merge(nums1,nums2,m,n)));
+    }
+    static int [] Merge(int [] nums1, int [] nums2, int m, int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m+n) - 1;
+        while (i >=0 && j >=0){
+            if (nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+        return nums1;
+    }
+}

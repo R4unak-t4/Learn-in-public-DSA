c7b68667b575f09abbcc9a098e78de7db6f7a9bf
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-15 20:33:31 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Binary/Search/ArrangingCoin.java
+++ src/main/java/Binary/Search/ArrangingCoin.java
@@ -5,7 +5,19 @@ public class ArrangingCoin {
 
     }
     static int BinarySearch(int n){
-
-
+        int s = 1, e = n;
+        while (s<=e){
+            int m = s+(e-s)/2;
+            int req = (m/2)*(m+1);
+            if (req == n){
+                return m;
+            }
+            else if (n<req){
+                e--;
+            }else {
+                s++;
+            }
+        }
+        return s;
     }
 }

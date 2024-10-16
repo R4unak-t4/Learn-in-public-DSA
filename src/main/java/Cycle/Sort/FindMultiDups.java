960d90778fe2538fd273e7b493bfcd5518d9012c
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-16 15:04:06 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Cycle/Sort/FindMultiDups.java
+++ src/main/java/Cycle/Sort/FindMultiDups.java
@@ -6,28 +6,28 @@ import java.util.List;
 //Question : https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
 public class FindMultiDups {
     public static void main(String[] args) {
-
+        int [] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(cycleSort(arr));
     }
     static List<Integer> cycleSort(int [] nums){
         int i = 0;
         List<Integer> ans = new ArrayList<>();
-        while(i < nums.length){
+        while (i<nums.length){
             int correct = nums[i]-1;
-            if (i == correct){
-                i++;
-            } else if (nums[i] == nums[correct]) {
-                if (ans.contains(nums[i])){
-                    i++;
-                }else {
-                    ans.add(nums[i]);
-                }
-
-            }else {
+            if (nums[i] != nums[correct]){
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
+            }else if (nums[i] == nums[correct]){
+                i++;
+            }else {
+                i++;
             }
-
+        }
+        for (int e = 0; e< nums.length;e++){
+            if (nums[e]-1 != e){
+                ans.add(nums[e]);
+            }
         }
         return ans;
     }

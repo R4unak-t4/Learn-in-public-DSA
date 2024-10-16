4877e02fb917b22420db10089f45a721e12c1a10
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-16 19:49:46 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Cycle/Sort/FindMinMIssing.java
+++ src/main/java/Cycle/Sort/FindMinMIssing.java
@@ -2,13 +2,14 @@ package Cycle.Sort;
 //Question : https://leetcode.com/problems/first-missing-positive/description/
 public class FindMinMIssing {
     public static void main(String[] args) {
-
+        int [] arr = {7,8,9,11,12};
+        System.out.println(cycleSort(arr));
     }
     static int cycleSort(int [] nums){
         int i = 0;
         while (i< nums.length){
             int correct = nums[i]-1;
-            if ((nums[i]< nums.length && nums[i]>0) && i!=correct){
+            if ((correct< nums.length && nums[i]>0) && i!=correct){
                 int temp =nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
@@ -17,11 +18,15 @@ public class FindMinMIssing {
                 i++;
             }
         }
+
         for (int e = 0; e< nums.length;e++){
+
             if (nums[e] != e+1){
-                return e+1;
+                    return e + 1;
+
+
             }
         }
-        return -1;
+        return nums.length+1;
     }
 }

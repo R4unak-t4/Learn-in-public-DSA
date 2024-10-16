65a22bcddadb5b0ab87db4c11a929a4e4d037a09
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-16 17:28:22 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Cycle/Sort/SetMismatch.java
+++ src/main/java/Cycle/Sort/SetMismatch.java
@@ -4,5 +4,18 @@ public class SetMismatch {
     public static void main(String[] args) {
 
     }
-
+    static int [] CycleSort(int [] nums){
+        int i = 0;
+        int [] ans ={-1,-1};
+        while (i< nums.length){
+            if(nums[i]-1 == i){
+                i++;
+            } else {
+                ans[0]= nums[i];
+                ans[1]= i+1;
+                return ans;
+            }
+        }
+        return ans;
+    }
 }

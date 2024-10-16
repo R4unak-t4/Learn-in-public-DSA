d5afb17c99d87b628e019467da71fe210d5b5696
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-16 18:09:26 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Cycle/Sort/SetMismatch.java
+++ src/main/java/Cycle/Sort/SetMismatch.java
@@ -1,19 +1,32 @@
 package Cycle.Sort;
+
+import java.util.Arrays;
+
 //Question : https://leetcode.com/problems/set-mismatch/description/
 public class SetMismatch {
     public static void main(String[] args) {
-
+        int [] arr = {1,2,2,4};
+        System.out.println(Arrays.toString(CycleSort(arr)));
     }
     static int [] CycleSort(int [] nums){
         int i = 0;
-        int [] ans ={-1,-1};
-        while (i< nums.length){
-            if(nums[i]-1 == i){
+        int [] ans = new int[2] ;
+        while (i< nums.length) {
+            int correct = nums[i]-1;
+            if (i ==correct){
                 i++;
-            } else {
-                ans[0]= nums[i];
-                ans[1]= i+1;
-                return ans;
+            }else if (nums[i] == nums[correct]){
+                i++;
+            }else {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+        }
+        for (int e = 0; e< nums.length;e++){
+            if (nums[e] != e+1 ){
+                ans[0] = nums[e];
+                ans[1] = e+1;
             }
         }
         return ans;

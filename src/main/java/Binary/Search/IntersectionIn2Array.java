fdd000321b7ab9a05f6ccf41bf24d7d1f54d5f51
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-18 20:10:23 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/Binary/Search/IntersectionIn2Array.java
@@ -0,0 +1,13 @@
+package Binary.Search;
+//Question : I lost the link and the question this will be updated after i find the question
+public class IntersectionIn2Array {
+    public static void main(String[] args) {
+        int [] num1 ={1,2,2,1}, num2 = {2,2};
+    }
+    static int [] BinarySearch(int [] num1, int [] num2){
+        if (num1.length > num2.length){
+
+        }
+        return null;
+    }
+}

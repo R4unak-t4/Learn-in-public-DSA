d516f17445dfcd02f2866ced57f0d00455f7a7fa
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-18 17:26:00 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/Linear/Search/MergerSort.java
@@ -0,0 +1,11 @@
+package Linear.Search;
+//Question : https://leetcode.com/problems/merge-intervals/description/
+public class MergerSort {
+    public static void main(String[] args) {
+        int [][] arr = {{1,3},{2,6},{8,10},{15,18}};
+    }
+    static int [] [] Linear(int [][] intervals){
+
+
+    }
+}

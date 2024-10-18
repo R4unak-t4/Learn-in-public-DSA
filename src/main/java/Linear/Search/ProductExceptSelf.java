6d5ad0a33c0f8fcd228fb24308de1822bbe3b164
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-18 20:10:55 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/Linear/Search/ProductExceptSelf.java
@@ -0,0 +1,23 @@
+package Linear.Search;
+//Question : https://leetcode.com/problems/product-of-array-except-self/description/
+//I am working on the solution
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static void main(String[] args) {
+    int [] arr = {1,2,3,4};
+        System.out.println(Arrays.toString(Linear(arr)));
+    }
+    static int [] Linear(int [] nums){
+        int[] answer = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++){
+            //for left
+            int LeftStart ;
+
+        }
+
+        return answer;
+
+    }
+}
+

03979434df426924dcb1a84183fed50bd30e9c84
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-18 20:11:19 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/Binary/Search/Sqrt.java
@@ -0,0 +1,9 @@
+package Binary.Search;
+//I am working on the solution
+//Question : https://leetcode.com/problems/sqrtx/description/
+public class Sqrt {
+    public static void main(String[] args) {
+        int x = 4;
+
+    }
+}

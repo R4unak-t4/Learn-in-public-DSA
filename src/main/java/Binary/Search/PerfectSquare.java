179687a0c9dc22f23abbc75f5da62af2c9486896
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-19 18:58:24 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/Binary/Search/PerfectSquare.java
+++ src/main/java/Binary/Search/PerfectSquare.java
@@ -1,4 +1,23 @@
 package Binary.Search;
 //Question :
 public class PerfectSquare {
+    public static void main(String[] args) {
+
+    }
+    static boolean BinarySearch(int num){
+        int s = 1, e = num;
+        while (s<= e){
+            int m = s+(e-s)/2;
+            if (m*m == num){
+                return true;
+            } else if (m*m > num) {
+                e = m-1;
+
+            }else {
+                s = m+1;
+            }
+        }
+        return false;
+
+    }
 }

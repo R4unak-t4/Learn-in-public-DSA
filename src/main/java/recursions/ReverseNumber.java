c1e633a23f0405c5e300352f74afcdeec6aae99f
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-22 11:54:47 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/recursions/ReverseNumber.java
+++ src/main/java/recursions/ReverseNumber.java
@@ -2,13 +2,13 @@ package recursions;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-
+        System.out.println(reverse(293));
     }
-    static int reverse(int num,int rev){
-        if (num == 0) {
-            return rev;
+    static int reverse(int num){
+        int digit = (int) Math.log10(num);
+        if (num<10){
+            return num;
         }
-        rev = rev * 10 + num % 10;
-        return reverse(num / 10, rev);
+        return (int) ((int) num%10 * Math.pow(10,digit) + reverse(num/10));
     }
 }

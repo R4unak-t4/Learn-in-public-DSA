3c9854dd5db6a4caa703b6cad1f4ea62d6e18b71
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-22 15:00:58 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/recursions/Leetcode1342.java
+++ src/main/java/recursions/Leetcode1342.java
@@ -5,16 +5,20 @@ public class Leetcode1342 {
         System.out.println(solution(14));
     }
     static int solution(int num){
-        int steps = 0;
-        while (num >=1 ){
-            if (num%2 == 0){
-                num = num/2;
-                steps++;
-            }else {
-                num = num -1;
-                steps++;
-            }
-        }
-        return steps;
+
     }
 }
+
+
+//by using iterations
+//int steps = 0;
+//        while (num >=1 ){
+//        if (num%2 == 0){
+//num = num/2;
+//steps++;
+//        }else {
+//num = num -1;
+//steps++;
+//        }
+//        }
+//        return steps;

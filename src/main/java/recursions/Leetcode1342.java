0253185cac6078501ff699d3003e8c0dbe83ad91
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-22 15:00:31 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/recursions/Leetcode1342.java
@@ -0,0 +1,20 @@
+package recursions;
+//Question : https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+public class Leetcode1342 {
+    public static void main(String[] args) {
+        System.out.println(solution(14));
+    }
+    static int solution(int num){
+        int steps = 0;
+        while (num >=1 ){
+            if (num%2 == 0){
+                num = num/2;
+                steps++;
+            }else {
+                num = num -1;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}

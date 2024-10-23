d68cd77dcb8ba68964cb359ff5bd471a9cb9d039
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-23 12:53:14 +0545

    DSA QUESTIONS WITH LINK

--- src/main/java/recursions/SortedArray.java
+++ src/main/java/recursions/SortedArray.java
@@ -2,15 +2,18 @@ package recursions;
 
 public class SortedArray {
     public static void main(String[] args) {
-        int [] arr = {1,2,4,5};
-        solution(arr,0);
+        int [] arr = {1,2,4,5,3};
+
+        System.out.println(solution(arr,0));
 
     }
     static boolean solution(int [] arr, int index){
         if (index == arr.length-1 ){
             return true;
         }
-        if (index < arr.length )
+        if (index < arr.length && arr[index] >arr[index+1]){
+            return false;
+        }
         return solution(arr,index+1);
 
 

c07b79b4901a54df75cbb764719ccde983ada878
Raunak-Xm89 <thapamagarraunak@gmail.com>
2024-10-27 11:20:57 +0545

    DSA QUESTIONS WITH LINK

---
+++ src/main/java/Subset/IterativeSubseq.java
@@ -0,0 +1,30 @@
+package Subset;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IterativeSubseq {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<int[]> subsequences = subseq(arr);
+
+        // Print each subsequence
+        for (int[] seq : subsequences) {
+            System.out.println(Arrays.toString(seq));
+        }
+    }
+
+    static List<int[]> subseq(int[] arr) {
+        List<int[]> ans = new ArrayList<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                ans.add(Arrays.copyOfRange(arr, i, j));  // Create subsequence from i to j-1
+            }
+        }
+
+        return ans;
+    }
+}
